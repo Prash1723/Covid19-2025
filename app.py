e2fe421561e191e226224c740e6ea22bcf6b79e1
Praveen S <49721746+Prash1723@users.noreply.github.com>
2025-05-31 23:51:57 +0530

    Update app\ data parser

---
+++ app.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+cases_url = 'https://www.covid19india.org/'
+active_cl = 'has-fixed-layout'
+
+response = requests.get(cases_url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+active_table = soup.find('table', attrs={'class': active_cl})
+
+# Load data
+cov = pd.read_html(str(active_table))[0]
+
+print(cov)
