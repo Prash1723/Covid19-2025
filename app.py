43b757afa8883c91e98db1c95c4739387a45d1ad
praveen <s.praveen795@gmail.com>
2025-06-04 23:49:03 +0530

    Update\ Total cases

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
 
 cases_url = 'https://www.covid19india.org/'
 active_cl = 'has-fixed-layout'
@@ -15,6 +18,14 @@ tables = soup.find_all('table', attrs={'class': active_cl})
 cov_total = pd.read_html(str(tables), header=0)[0]
 cov_active = pd.read_html(str(tables), header=0)[1]
 
-cov_total.to_csv('data/total_data.csv', index=False)
+# Rename Columns
+cov_total.columns = ["s.no", "state", "total_active_cases", "new_cases_since_day_before"]
 
+# Save Data
+cov_total.to_csv('data/total_data.csv', index=False)
 cov_active.to_csv('data/daily_data.csv', index=False)
+
+cov_total.plot(kind='barh', y="total_active_cases", x="state", width=0.9)
+plt.xlabel('Total Active Cases')
+plt.ylabel('State & UTs')
+plt.show()
